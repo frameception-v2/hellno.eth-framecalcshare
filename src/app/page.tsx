c09dbc81bffd2884b22067bafea78d0333673a66
hellno <hellno@users.noreply.github.com>
2025-01-17 21:40:10 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { Metadata } from "next";
 import App from "./app";
 import { PROJECT_TITLE, PROJECT_DESCRIPTION } from "~/lib/constants";
 
-const appUrl = process.env.NEXT_PUBLIC_URL;
+const appUrl =
+  process.env.NEXT_PUBLIC_URL ||
+  `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
 const frame = {
   version: "next",
